e866f16fe9c88565f2fdeb4923ec8f3c6777cb78
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-04-22 21:17:42 -0600

    Added babel module

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
 };
